bf2b4c91f6ffe6b136aa963191d895cf95bcf8f4
rocadenis <119231290+rocadenis@users.noreply.github.com>
2024-12-08 18:50:04 +0200

    register

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css', '/src/styles.css'],
 })
 export class RegisterComponent {
+  fullName: string = '';
+  phoneNumber: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    console.log('Full Name:', this.fullName);
+    console.log('Phone Number:', this.phoneNumber);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+
+  navigateHome() {
+    this.router.navigate(['/']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToExplore() {
+    this.router.navigate(['/explore']);
+  }
+
+  navigateToSearch() {
+    this.router.navigate(['/search']);
+  }
+
+  scrollToAboutSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
