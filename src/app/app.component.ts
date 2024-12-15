1816a4fd1f7853a9f8c9f10dcff1be79b8f4d117
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-12-15 12:17:37 +0200

    Add GSAP and Lenis for smooth scrolling; implement lifecycle hooks in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { OnInit, OnDestroy, ElementRef, AfterViewInit } from '@angular/core';
+import Lenis from 'lenis';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +12,40 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   title = 'real-estate-management-frontend';
+  private lenis: Lenis | undefined;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private elementRef: ElementRef) {}
 
+  ngOnInit() : void {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      this.lenis = new Lenis({
+        duration: 1.2,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true,
+        orientation: 'vertical',
+      });
+  
+      this.lenis.on('scroll', () => {
+        ScrollTrigger.update(); 
+      });
+  
+      const raf = (time: number) => {
+        this.lenis?.raf(time);
+        requestAnimationFrame(raf);
+      };
+      requestAnimationFrame(raf);
+    }
+  }
+  
+
+  ngOnDestroy(): void {
+    this.lenis?.destroy();
+    gsap.ticker.remove((time) => this.lenis?.raf(time * 1000));
+  }
 }
